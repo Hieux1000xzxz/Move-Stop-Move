5571e66e3b7d26e7d57bbc5bcbadc93f3e388515
Le Tran Gia Bao <letrangiabao18062k5@gmail.com>
2025-09-11 16:00:25 +0700

    Change powerup

--- Assets/Scripts/Pooling/ObjectPool.cs
+++ Assets/Scripts/Pooling/ObjectPool.cs
@@ -10,14 +10,14 @@ public class Preallocation
     public bool expandable;
     public ObjectType type;
     public WeaponType weaponType;
-    public PowerupType powerupType;
+    //public PowerupType powerupType;
 }
 
 public enum ObjectType
 {
     Enemy,
     Weapon,
-    Powerup,
+    //Powerup,
     Other
 }
 
@@ -46,7 +46,7 @@ public class ObjectPool : Singleton<ObjectPool>
         {
             for (int i = 0; i < item.count; ++i)
             {
-                pooledGobjects.Add(CreateGobject(item.gameObject, item.type));
+                pooledGobjects.Add(CreateGobject(item.gameObject));
             }
         }
     }
@@ -238,88 +238,88 @@ public class ObjectPool : Singleton<ObjectPool>
         return null;
     }
 
-    private GameObject CreateGobject(GameObject item, ObjectType type = ObjectType.Other)
+    private GameObject CreateGobject(GameObject item)
     {
         GameObject gobject = Instantiate(item, transform);
         gobject.transform.SetParent(transform);
 
-        if (type == ObjectType.Powerup)
-        {
-            gobject.SetActive(true);
-        }
-        else
-        {
+        //if (type == ObjectType.Powerup)
+        //{
+        //    gobject.SetActive(true);
+        //}
+        //else
+        //{
             gobject.transform.position = new Vector3(9999, 9999, 9999);
             gobject.SetActive(false);
-        }
+        //}
 
         return gobject;
     }
 
 
-    #region Powerup
-    public GameObject SpawnPowerup(PowerupType type, Transform spawnPoint = null)
-    {
-        GameObject obj = GetInactivePowerup(type);
+    //#region Powerup
+    //public GameObject SpawnPowerup(PowerupType type, Transform spawnPoint = null)
+    //{
+    //    GameObject obj = GetInactivePowerup(type);
 
-        if (obj == null)
-        {
-            obj = ExpandPoolPowerup(type);
-        }
+    //    if (obj == null)
+    //    {
+    //        obj = ExpandPoolPowerup(type);
+    //    }
 
-        if (obj == null) return null;
+    //    if (obj == null) return null;
 
-        obj.SetActive(true); 
+    //    obj.SetActive(true); 
 
-        if (spawnPoint != null)
-        {
-            obj.transform.SetParent(null);
-            obj.transform.position = spawnPoint.position;
-            obj.transform.rotation = spawnPoint.rotation;
-        }
+    //    if (spawnPoint != null)
+    //    {
+    //        obj.transform.SetParent(null);
+    //        obj.transform.position = spawnPoint.position;
+    //        obj.transform.rotation = spawnPoint.rotation;
+    //    }
 
-        var netObj = obj.GetComponent<NetworkObject>();
-        if (netObj != null && !netObj.IsSpawned && NetworkManager.Singleton.IsServer)
-        {
-            netObj.Spawn(true);
-        }
+    //    var netObj = obj.GetComponent<NetworkObject>();
+    //    if (netObj != null && !netObj.IsSpawned && NetworkManager.Singleton.IsServer)
+    //    {
+    //        netObj.Spawn(true);
+    //    }
 
-        return obj;
-    }
+    //    return obj;
+    //}
 
 
-    private GameObject GetInactivePowerup(PowerupType type)
-    {
-        foreach (var obj in pooledGobjects)
-        {
-            if (!obj.activeSelf)
-            {
-                foreach (var pre in preAllocations)
-                {
-                    if (pre.type == ObjectType.Powerup && pre.powerupType == type &&
-                        obj.name.Contains(pre.gameObject.name))
-                    {
-                        return obj;
-                    }
-                }
-            }
-        }
-        return null;
-    }
+    //private GameObject GetInactivePowerup(PowerupType type)
+    //{
+    //    foreach (var obj in pooledGobjects)
+    //    {
+    //        if (!obj.activeSelf)
+    //        {
+    //            foreach (var pre in preAllocations)
+    //            {
+    //                if (pre.type == ObjectType.Powerup && pre.powerupType == type &&
+    //                    obj.name.Contains(pre.gameObject.name))
+    //                {
+    //                    return obj;
+    //                }
+    //            }
+    //        }
+    //    }
+    //    return null;
+    //}
 
-    private GameObject ExpandPoolPowerup(PowerupType type)
-    {
-        foreach (var pre in preAllocations)
-        {
-            if (pre.type == ObjectType.Powerup && pre.powerupType == type && pre.expandable)
-            {
-                GameObject newPowerup = CreateGobject(pre.gameObject);
-                pooledGobjects.Add(newPowerup);
-                return newPowerup;
-            }
-        }
-        return null;
-    }
+    //private GameObject ExpandPoolPowerup(PowerupType type)
+    //{
+    //    foreach (var pre in preAllocations)
+    //    {
+    //        if (pre.type == ObjectType.Powerup && pre.powerupType == type && pre.expandable)
+    //        {
+    //            GameObject newPowerup = CreateGobject(pre.gameObject);
+    //            pooledGobjects.Add(newPowerup);
+    //            return newPowerup;
+    //        }
+    //    }
+    //    return null;
+    //}
 
-    #endregion
+    //#endregion
 }
--- Assets/Scripts/PowerUp/Powerup.cs
+++ Assets/Scripts/PowerUp/Powerup.cs
@@ -1,37 +1,37 @@
-﻿using UnityEngine;
-using Unity.Netcode;
+﻿//using UnityEngine;
+//using Unity.Netcode;
 
-public class Powerup : NetworkBehaviour
-{
-    [Header("Config")]
-    [SerializeField] public PowerupType type;
-    [SerializeField] public float duration = 5f;
+//public class Powerup : NetworkBehaviour
+//{
+//    [Header("Config")]
+//    [SerializeField] public PowerupType type;
+//    [SerializeField] public float duration = 5f;
 
-    [Header("Refs")]
-    [SerializeField] public Collider triggerCollider;
-    [SerializeField] public NetworkObject netObject;
+//    [Header("Refs")]
+//    [SerializeField] public Collider triggerCollider;
+//    [SerializeField] public NetworkObject netObject;
 
-    public PowerupType Type => type;
-    public float Duration => duration;
+//    public PowerupType Type => type;
+//    public float Duration => duration;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (!IsServer) return;
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        if (!IsServer) return;
 
-        CharacterBase character = other.GetComponent<CharacterBase>();
-        if (character != null)
-        {
-            character.ApplyPowerup(type, duration);
+//        CharacterBase character = other.GetComponent<CharacterBase>();
+//        if (character != null)
+//        {
+//            character.ApplyPowerup(type, duration);
 
-            if (netObject != null && netObject.IsSpawned)
-            {
-                netObject.Despawn();
-            }
-        }
-    }
+//            if (netObject != null && netObject.IsSpawned)
+//            {
+//                netObject.Despawn();
+//            }
+//        }
+//    }
 
-    public void SetType(PowerupType newType)
-    {
-        type = newType;
-    }
-}
+//    public void SetType(PowerupType newType)
+//    {
+//        type = newType;
+//    }
+//}
