7f1bbd31b2b06b95e3ab861b40adace1e6a4f7f0
Hieu Truong <truongduchieux6qt@gmail.com>
2025-10-04 11:31:26 +0700

    fix coin

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -202,7 +202,7 @@ public class GameManager : NetworkBehaviour
                 }
                 else
                 {
-                    GameWinClientRpc();
+                    GameWinClientRpc();   
                 }
             }
         }
